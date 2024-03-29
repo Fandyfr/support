65cbcfc24228189023ff3303a67c1743f4be2b9b
Fandy Fathurrohman <fandyfafa09@gmail.com>
2024-03-29 16:12:24 +0800

    support me
    Jangan Lupa Dukung Saya Di Saweria dan GitHub Sponsors

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// var typingEffect = new Typed(".typedtext",{
+//     strings : [" Terima kasih atas dukungan Anda."],
+//     loop : true,
+//     typeSpeed : 90, 
+//     backSpeed : 90,
+//     backDelay : 1500
+// })
